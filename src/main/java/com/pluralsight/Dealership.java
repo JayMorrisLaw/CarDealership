297df7d7ece937bfe1cc293c8becb095047fe68c
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-05-12 00:19:48 -0400

    updated dealership methods and added tostring

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Dealership {
@@ -22,14 +23,65 @@ public class Dealership {
         return this.inventory;
     }
     // methods for search, return null for now
-    public ArrayList<Vehicle> searchByPrice(double min, double max) { return null; }
-    public ArrayList<Vehicle> searchByMakeModel(String make, String model) { return null; }
-    public ArrayList<Vehicle> searchByYear(int min, int max) { return null; }
-    public ArrayList<Vehicle> searchByColor(String color) { return null; }
-    public ArrayList<Vehicle> searchByMileage(int min, int max) { return null; }
-    public ArrayList<Vehicle> searchByType(String type) { return null; }
+    public ArrayList<Vehicle> searchByPrice(double min, double max){
+      ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max){
+                results.add(vehicle);
+            }
+        }
+        return results;
+    }
+    public ArrayList<Vehicle> searchByMakeModel(String make, String model) {
+        ArrayList<Vehicle> MakeModel = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)){
+                MakeModel.add(vehicle);
+            }
+        }
+        return MakeModel;
+    }
+    public ArrayList<Vehicle> searchByYear(int min, int max) {
+        ArrayList<Vehicle> YearSearch = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max){
+                YearSearch.add(vehicle);
+            }
+        }
+        return YearSearch;
+    }
+    public ArrayList<Vehicle> searchByColor(String color) {
+        ArrayList<Vehicle> ColorSearch = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getColor().equalsIgnoreCase(color)){
+                ColorSearch.add(vehicle);
+            }
+        }
+        return ColorSearch;
+    }
+    public ArrayList<Vehicle> searchByMileage(int min, int max) {
+        ArrayList<Vehicle> MileSearch = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getOdometer() >= min && vehicle.getOdometer() <= max){
+                MileSearch.add(vehicle);
+            }
+        }
+        return MileSearch;
+    }
+    public ArrayList<Vehicle> searchByType(String type) {
+        ArrayList<Vehicle> TypeSearch = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getVehicleType().equalsIgnoreCase(type)){
+                TypeSearch.add(vehicle);
+            }
+        }
+        return TypeSearch;
+    }
 
     public void removeVehicle(Vehicle vehicle){
 
     }
+
+
+
 }
