415c2ddf1c2e1fd888ea771da82cea502e12a6d7
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-05-14 13:52:23 -0400

    updating car dealership UI class

---
+++ src/main/java/com/pluralsight/Programs.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class Programs {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,7 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
-    public void display(){
-        
+    private Scanner scanner;
+    private Dealership dealership;
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealership();
+        if (dealership == null){
+            System.out.println("Error: file not found!");
+            return;
+        }
     }
+
+    public void display(){
+        // display menu
+        int choice  = 0;
+        while(choice !=99){
+
+        }
+
+
+    }
+
 }
